e9eb4bd4eca2e0fb8544b5ee96dcf33a1f46f267
Amitoz Deol <amitoz.deol@eawelectro.com>
2018-12-29 16:34:06 -0500

    Added service worker. Added manifest.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,32 @@
-var $ = function( elem ) { return document.querySelector( elem ); };
-var $$ = function( elem ) { return document.querySelectorAll( elem ); };
-console.log("testing");
+var $ = function (elem) {
+    return document.querySelector(elem);
+};
+var $$ = function (elem) {
+    return document.querySelectorAll(elem);
+};
+//This is the "Offline copy of pages" service worker
+
+//Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+if (navigator.serviceWorker.controller) {
+    console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+    //Register the ServiceWorker
+    navigator.serviceWorker.register('pwabuilder-sw.js', {
+        scope: './'
+    }).then(function (reg) {
+        console.log('Service worker has been registered for scope:' + reg.scope);
+    });
+}
+
 
 window.addEventListener('load', lazyloading, false);
-function lazyloading(){
-    var lazyloadImages;    
+
+function lazyloading() {
+    var lazyloadImages;
     if ("IntersectionObserver" in window) {
         lazyloadImages = $$(".lazy");
-        var imageObserver = new IntersectionObserver(function(entries, observer) {
-            entries.forEach(function(entry) {
+        var imageObserver = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function (entry) {
                 if (entry.isIntersecting) {
                     var image = entry.target;
                     image.src = image.dataset.src;
@@ -18,27 +36,27 @@ function lazyloading(){
             });
         });
 
-        lazyloadImages.forEach(function(image) {
+        lazyloadImages.forEach(function (image) {
             imageObserver.observe(image);
         });
-    } else {  
+    } else {
         var lazyloadThrottleTimeout;
         lazyloadImages = $$(".lazy");
-        
-        function lazyload () {
-            if(lazyloadThrottleTimeout) {
-                clearTimeout(lazyloadThrottleTimeout);
-            }    
 
-            lazyloadThrottleTimeout = setTimeout(function() {
+        function lazyload() {
+            if (lazyloadThrottleTimeout) {
+                clearTimeout(lazyloadThrottleTimeout);
+            }
+
+            lazyloadThrottleTimeout = setTimeout(function () {
                 var scrollTop = window.pageYOffset;
-                lazyloadImages.forEach(function(img) {
-                    if(img.offsetTop < (window.innerHeight + scrollTop)) {
-                    img.src = img.dataset.src;
-                    img.classList.remove('lazy');
+                lazyloadImages.forEach(function (img) {
+                    if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                        img.src = img.dataset.src;
+                        img.classList.remove('lazy');
                     }
                 });
-                if(lazyloadImages.length == 0) { 
+                if (lazyloadImages.length == 0) {
                     document.removeEventListener("scroll", lazyload);
                     window.removeEventListener("resize", lazyload);
                     window.removeEventListener("orientationChange", lazyload);
@@ -51,7 +69,7 @@ function lazyloading(){
         window.addEventListener("orientationChange", lazyload);
     }
 }
- 
+
 // Remove no-js class
 $('html').classList.remove('no-js');
 
@@ -63,12 +81,12 @@ $$('#menu a[href^="#"]').forEach(anchor => {
             top: $(this.getAttribute('href')).getBoundingClientRect().top,
             behavior: 'smooth'
         })
-        if($('header').classList.contains('active')){
+        if ($('header').classList.contains('active')) {
             $$('header, body').forEach(e => e.classList.remove('active'));
         }
     });
 });
- 
+
 // Scroll to top
 $('#to-top').addEventListener('click', function (e) {
     window.scrollTo({
@@ -89,4 +107,3 @@ $('#lead-down span').addEventListener('click', function (e) {
 $('#mobile-menu-open').addEventListener('click', function (e) {
     $$('header, body').forEach(e => e.classList.add('active'));
 });
-
