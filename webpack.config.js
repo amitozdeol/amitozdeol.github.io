96a3af8e738904cb1cc075a891f1244dec28dffe
amitoz deol <amitozdeol@amitozs-MacBook-Pro.local>
2018-12-30 12:44:12 -0500

    Increaded audit score. using system font. Used other best practices

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
-const path = require('path');
+const path = require("path");
 var config = {
-    entry: {
-        app: './js/scripts.js'
-    },
-    output:{
-        path: path.resolve(__dirname, 'build'), 
-        filename: 'app.js'
-    },
-    module:{
-        rules: [{
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }
-        }]
-    }
-}
+  entry: {
+    app: "./js/scripts.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "app.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
 module.exports = (env, argv) => {
-    config.devtool = argv.mode === 'production' ? 'eval' : 'inline-source-map';
-    return config;
+  config.devtool = argv.mode === "production" ? "eval" : "inline-source-map";
+  return config;
 };
