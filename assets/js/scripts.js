a4ccd6860f0e1434e98d3146e44c3db85f2ff3e4
Amitoz Deol <adeol@KOG-C02X267KJG5M.local>
2019-12-07 23:27:53 -0500

    Added dark mode to the entire site.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,13 @@ var $$ = function (elem) {
   return document.querySelectorAll(elem);
 };
 
-//This is the "Offline copy of pages" service worker
+$("html").classList.remove("no-js"); // Remove no-js class
+$("#toggle-switch").checked = false; //Keep toggle switch unchecked
+
+/**
+ * Service Worker - Offline support ==========================================
+ * This is the "Offline copy of pages" service worker
+ */
 if (navigator.serviceWorker.controller) {
   console.log("[PWA Builder] active service worker found, no need to register");
 } else {
@@ -19,6 +25,89 @@ if (navigator.serviceWorker.controller) {
     });
 }
 
+/**
+ * NAV ===================================================
+ * Animate to section when nav is clicked
+ */
+$$('#menu a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: $(this.getAttribute("href")).getBoundingClientRect().top,
+      behavior: "smooth"
+    });
+    if ($("header").classList.contains("active")) {
+      $$("header, body").forEach(e => e.classList.remove("active"));
+    }
+  });
+});
+
+// Scroll to top
+$("#to-top").addEventListener("click", function (e) {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+// Scroll to first element
+$("#lead-down span").addEventListener("click", function (e) {
+  window.scrollTo({
+    top: $("#about").getBoundingClientRect().top,
+    behavior: "smooth"
+  });
+});
+
+// Open mobile menu
+$("#mobile-menu-open").addEventListener("click", function (e) {
+  $$("header, body").forEach(e => e.classList.add("active"));
+});
+
+// Remove mobile menu
+$("#mobile-menu-close").addEventListener("click", function (e) {
+  $$("header, body").forEach(e => e.classList.remove("active"));
+});
+
+/**
+ * Copyright year ================================================
+ */
+$("#CR-year").innerHTML = new Date().getFullYear();
+
+/**
+ * Dark mode fevicon ==============================================
+ * replace fevicon with dark/light themes as appropriate
+ */
+var darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+darkModeMediaQuery.addListener(handleDarkmodeFav);
+handleDarkmodeFav(darkModeMediaQuery);
+
+function handleDarkmodeFav(e) {
+  let darkModeOn = e.matches; // true if dark mode is enabled
+  let favicon = $('link[rel="icon"]');
+  favicon.href = darkModeOn ? '../assets/images/AD_logo_white.png' : '../assets/images/AD_logo_dark1.png';
+}
+
+/**
+ * Dark mode CSS =================================================
+ * dark mode switch
+ */
+$("#toggle-switch").addEventListener("click", toggleDarkMode);
+
+function toggleDarkMode(e) {
+  if (e.target.checked) {
+    $('body').classList.add('dark-mode');
+    $("link[href='assets/css/header.css']").href = "assets/css/header-dark.css";
+    $("link[href='assets/css/styles.css']").href = "assets/css/styles-dark.css";
+  } else {
+    $('body').classList.remove('dark-mode');
+    $("link[href='assets/css/header-dark.css']").href = "assets/css/header.css";
+    $("link[href='assets/css/styles-dark.css']").href = "assets/css/styles.css";
+  }
+}
+
+/**
+ * Lazy load Images ==============================================
+ */
 window.addEventListener("load", lazyloading, false);
 
 function lazyloading() {
@@ -69,57 +158,3 @@ function lazyloading() {
     window.addEventListener("orientationChange", lazyload);
   }
 }
-
-// Remove no-js class
-$("html").classList.remove("no-js");
-
-// Animate to section when nav is clicked
-$$('#menu a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    window.scrollTo({
-      top: $(this.getAttribute("href")).getBoundingClientRect().top,
-      behavior: "smooth"
-    });
-    if ($("header").classList.contains("active")) {
-      $$("header, body").forEach(e => e.classList.remove("active"));
-    }
-  });
-});
-
-// Scroll to top
-$("#to-top").addEventListener("click", function (e) {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-});
-
-// Scroll to first element
-$("#lead-down span").addEventListener("click", function (e) {
-  window.scrollTo({
-    top: $("#about").getBoundingClientRect().top,
-    behavior: "smooth"
-  });
-});
-
-// Open mobile menu
-$("#mobile-menu-open").addEventListener("click", function (e) {
-  $$("header, body").forEach(e => e.classList.add("active"));
-});
-
-//Copyright year
-$("#CR-year").innerHTML = new Date().getFullYear();
-
-//Dark mode fevicon
-var darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-
-handleDarkmode(darkModeMediaQuery);
-
-function handleDarkmode(e) {
-  let darkModeOn = e.matches; // true if dark mode is enabled
-  let favicon = $('link[rel="icon"]'); // get favicon.ico element
-  // replace icons with dark/light themes as appropriate
-  favicon.href = darkModeOn ? '../assets/images/AD_logo_white.png' : '../assets/images/AD_logo_dark1.png';
-}
-darkModeMediaQuery.addListener(handleDarkmode);
