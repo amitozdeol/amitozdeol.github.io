423f36b0bdfbaca19b54ae0d445b7bc4d987f717
Amitoz Deol <adeol@KOG-C02X267KJG5M.local>
2020-04-18 18:03:05 -0400

    Added Tick fitness project to index.html

---
+++ project/vintage/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    important: true,
+    theme: {
+      fontFamily: {
+        display: ['Walter', 'sans-serif'],
+        body: ['PortLligatSlab', 'sans-serif'],
+      },
+      extend: {
+        colors: {
+          yellow: '#c8a273',
+        },
+        margin: {
+          '96': '24rem',
+          '128': '32rem',
+        },
+      }
+    },
+    variants: {
+      opacity: ['responsive', 'hover']
+    }
+  }
