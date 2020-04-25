dc84515d42682e0b77f10e46ec254dc229575826
amitoz deol <amitozdeol@amitozs-MacBook-Pro.local>
2020-04-24 23:02:01 -0400

    webpack improvement. added purgecss.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,43 +12,43 @@ $("#toggle-switch").checked = false; //Keep toggle switch unchecked
  * NAV ===================================================
  * Animate to section when nav is clicked
  */
-$$('#menu a[href^="#"]').forEach(anchor => {
+$$('#menu a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
     window.scrollTo({
       top: $(this.getAttribute("href")).getBoundingClientRect().top,
-      behavior: "smooth"
+      behavior: "smooth",
     });
     if ($("header").classList.contains("active")) {
-      $$("header, body").forEach(e => e.classList.remove("active"));
+      $$("header, body").forEach((e) => e.classList.remove("active"));
     }
   });
 });
 
 // Scroll to top
-$("#to-top").addEventListener("click", function (e) {
+$("#to-top").addEventListener("click", function () {
   window.scrollTo({
     top: 0,
-    behavior: "smooth"
+    behavior: "smooth",
   });
 });
 
 // Scroll to first element
-$("#lead-down span").addEventListener("click", function (e) {
+$("#lead-down span").addEventListener("click", function () {
   window.scrollTo({
     top: $("#about").getBoundingClientRect().top,
-    behavior: "smooth"
+    behavior: "smooth",
   });
 });
 
 // Open mobile menu
-$("#mobile-menu-open").addEventListener("click", function (e) {
-  $$("header, body").forEach(e => e.classList.add("active"));
+$("#mobile-menu-open").addEventListener("click", function () {
+  $$("header, body").forEach((e) => e.classList.add("active"));
 });
 
 // Remove mobile menu
-$("#mobile-menu-close").addEventListener("click", function (e) {
-  $$("header, body").forEach(e => e.classList.remove("active"));
+$("#mobile-menu-close").addEventListener("click", function () {
+  $$("header, body").forEach((e) => e.classList.remove("active"));
 });
 
 /**
@@ -60,14 +60,16 @@ $("#CR-year").innerHTML = new Date().getFullYear();
  * Dark mode fevicon ==============================================
  * replace fevicon with dark/light themes as appropriate
  */
-var darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+var darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 darkModeMediaQuery.addListener(handleDarkmodeFav);
 handleDarkmodeFav(darkModeMediaQuery);
 
 function handleDarkmodeFav(e) {
   let darkModeOn = e.matches; // true if dark mode is enabled
   let favicon = $('link[rel="icon"]');
-  favicon.href = darkModeOn ? '../assets/images/AD_logo_white.png' : '../assets/images/AD_logo_dark1.png';
+  favicon.href = darkModeOn
+    ? "../../assets/images/AD_logo_white.png"
+    : "../../assets/images/AD_logo_dark1.png";
 }
 
 /**
@@ -78,13 +80,13 @@ $("#toggle-switch").addEventListener("click", toggleDarkMode);
 
 function toggleDarkMode(e) {
   if (e.target.checked) {
-    $('body').classList.add('dark-mode');
-    $("link[href='assets/css/header.css']").href = "assets/css/header-dark.css";
-    $("link[href='assets/css/styles.css']").href = "assets/css/styles-dark.css";
+    $("body").classList.add("dark-mode");
+    $("link[href='build/css/style-light.css']").href =
+      "build/css/style-dark.css";
   } else {
-    $('body').classList.remove('dark-mode');
-    $("link[href='assets/css/header-dark.css']").href = "assets/css/header.css";
-    $("link[href='assets/css/styles-dark.css']").href = "assets/css/styles.css";
+    $("body").classList.remove("dark-mode");
+    $("link[href='build/css/style-dark.css']").href =
+      "build/css/style-light.css";
   }
 }
 
@@ -97,7 +99,7 @@ function lazyloading() {
   var lazyloadImages;
   if ("IntersectionObserver" in window) {
     lazyloadImages = $$(".lazy");
-    var imageObserver = new IntersectionObserver(function (entries, observer) {
+    var imageObserver = new IntersectionObserver(function (entries) {
       entries.forEach(function (entry) {
         if (entry.isIntersecting) {
           var image = entry.target;
@@ -115,7 +117,7 @@ function lazyloading() {
     var lazyloadThrottleTimeout;
     lazyloadImages = $$(".lazy");
 
-    function lazyload() {
+    const lazyload = function () {
       if (lazyloadThrottleTimeout) {
         clearTimeout(lazyloadThrottleTimeout);
       }
@@ -134,7 +136,7 @@ function lazyloading() {
           window.removeEventListener("orientationChange", lazyload);
         }
       }, 20);
-    }
+    };
 
     document.addEventListener("scroll", lazyload);
     window.addEventListener("resize", lazyload);
@@ -152,7 +154,7 @@ if (navigator.serviceWorker.controller) {
   //Register the ServiceWorker
   navigator.serviceWorker
     .register("sw.js", {
-      scope: "./"
+      scope: "./",
     })
     .then(function (reg) {
       console.log("Service worker has been registered for scope:" + reg.scope);
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,41 @@
+/* eslint-disable no-undef */
 const path = require("path");
-const markdownPlugin = require('markdown-html-webpack-plugin');
+const glob = require("glob");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+
+const PATHS = {
+  src: path.join(__dirname),
+};
 
 var config = {
   entry: {
+    common: ["./assets/scss/dark-mode.scss", "./assets/scss/bootstrap.scss"],
+    "style-light": ["./assets/scss/styles.scss", "./assets/scss/header.scss"],
+    "style-dark": [
+      "./assets/scss/styles-dark.scss",
+      "./assets/scss/header-dark.scss",
+    ],
     app: "./assets/js/scripts.js",
-    project: "./assets/js/project.js"
+    project: "./assets/js/project.js",
   },
   output: {
     path: path.resolve(__dirname, "build"),
-    publicPath: path.join(__dirname, 'build'),
-    filename: "[name].js"
+    publicPath: path.join(__dirname, "build"),
+    filename: "js/[name].js",
   },
   devServer: {
-    contentBase: './',
+    contentBase: "./",
     http2: true,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      "Access-Control-Allow-Origin": "*",
     },
   },
-  watch: true,
+  stats: {
+    entrypoints: false,
+    children: false,
+  },
+  watch: false,
   module: {
     rules: [
       {
@@ -27,28 +44,53 @@ var config = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: [["@babel/preset-env", {
-              "targets": {
-                "node": "current"
-              }
-            }]]
-          }
-        }
-      }
-    ]
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    node: "current",
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { url: false } }, // translates CSS into CommonJS modules
+          "sass-loader", // compiles Sass to CSS
+        ],
+      },
+    ],
   },
   plugins: [
-    new markdownPlugin({
-      filePath: '../docs/md',
-      exportPath: '../docs/html/',
-      isEncodeName: false, // if need to encode file name, like chinese
-      template: '../docs/template.html'
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "css/[name].css",
     }),
-  ]
+  ],
+  resolve: {
+    extensions: [".js"],
+  },
 };
+
 module.exports = (env, argv) => {
   if (argv.mode != "production") {
     config.devtool = "inline-source-map";
+  }
+
+  //Purge css on production
+  if (argv.mode === "production") {
+    config.plugins.push(
+      new PurgecssPlugin({
+        paths: glob.sync(`${PATHS.src}/*`, { nodir: true }),
+        content: ["index.html"],
+      })
+    );
   }
   return config;
 };
