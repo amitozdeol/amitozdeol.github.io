5e802627ce975b7e42843327c4051fa7d28dcab3
amitoz deol <amitozdeol@amitozs-MacBook-Pro.local>
2021-04-06 14:18:42 -0400

    removed action file

--- .scripts/pre-commit.sh
+++ .scripts/pre-commit.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# Run production command when we make a commit on master branch
 
 current_branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
 if [ 'master' = ${current_branch} ]
 then
     npm run prod;
-    exit 0 # push will not execute
+    exit 0 # commit will execute
 else
   exit 0 # push will execute
 fi
